6e41f75d69758d0190a1bff8c4e8873b4fb65ad6
Grokhen <152092079+Grokhen@users.noreply.github.com>
2024-02-08 17:41:34 +0000

    user data entry added

--- src/app.js
+++ src/app.js
@@ -6,22 +6,59 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["cat", "racoon"];
-  let domainType = [".com", ".es", ".net", ".io"];
+  let pronoun = [];
+  let adj = [];
+  let noun = [];
+  let domainType = [];
+  let domainsList = document.getElementById("domains");
+  for (let i = 0; i < 3; i++) {
+    let usersPronouns = prompt(`Escribe un pronombre ${i + 1}/3:`);
+    if (usersPronouns === null || usersPronouns === "") {
+      alert("Debe ingresar un valor.");
+      i--;
+    } else {
+      pronoun.push(usersPronouns);
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    let usersAdj = prompt(`Escribe un adjetivo ${i + 1}/3:`);
+    if (usersAdj === null || usersAdj === "") {
+      alert("Debe ingresar un valor.");
+      i--;
+    } else {
+      adj.push(usersAdj);
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    let usersNoun = prompt(`Escribe un nombre ${i + 1}/3:`);
+    if (usersNoun === null || usersNoun === "") {
+      alert("Debe ingresar un valor.");
+      i--;
+    } else {
+      noun.push(usersNoun);
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    let usersDomainType = prompt(`Escribe un tipo de dominio ${i + 1}/3:`);
+    if (usersDomainType === null || usersDomainType === "") {
+      alert("Debe ingresar un valor.");
+      i--;
+    } else {
+      domainType.push(usersDomainType);
+    }
+  }
 
   for (let p = 0; p < pronoun.length; p++) {
     for (let a = 0; a < adj.length; a++) {
       for (let n = 0; n < noun.length; n++) {
         for (let d = 0; d < domainType.length; d++) {
-          console.log(pronoun[p] + adj[a] + noun[n] + domainType[d]);
+          let newDomains = document.createElement(`li`);
+          newDomains.classList.add("list-group-item");
+          newDomains.textContent =
+            pronoun[p] + noun[n] + adj[a] + domainType[d];
+          domainsList.appendChild(newDomains);
         }
       }
     }
   }
-  console.log("Hello Rigo from the console!");
 };
-
-//Domain name generator code
